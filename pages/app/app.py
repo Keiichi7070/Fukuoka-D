996a3c6f82cd027421869d62ccff6ef1d3476258
keiichi7070 <keyiii7070@gmail.com>
2019-12-17 23:23:06 +0900

    pages4

--- pages/app/app.py
+++ pages/app/app.py
@@ -4,6 +4,8 @@ import numpy as np
 import os
 from werkzeug.utils import secure_filename
 from sklearn.externals import joblib
+import re
+from PIL import Image
 
 # 画像のアップロード先のディレクトリ
 UPLOAD_FOLDER = '../uploads'
@@ -14,35 +16,71 @@ app = Flask(__name__)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+path = '../uploads'
+
+files = []
+
+for filename in os.listdir(path):
+         if os.path.isfile(os.path.join(path, filename)):  #ファイルのみ取得
+                     files.append(filename)
+
 def allwed_file(filename):
     # .があるかどうかのチェックと、拡張子の確認
     # OKなら１、だめなら0
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+filenames = os.listdir('./')
+imgl=[]
+ww=[]
+hh=[]
+for fname in sorted(filenames):
+    path, ext = os.path.splitext( os.path.basename(fname) )
+    if ext=='.JPG' and path[0:2]!='._':
+        pic=path+ext
+        im=Image.open(pic)
+        w=im.size[0]
+        h=im.size[1]
+        print(pic, w, h)
+        imgl=imgl+[pic]
+        ww=ww+[w]
+        hh=hh+[h]
+
+f=open('maggie.html','w')
+print('<html>',file=f)
+print('<body>',file=f)
+print('<table>',file=f)
+n=len(imgl)
+m=int(n/5)+1
+k=-1
+for i in range(0,m):
+    print('<tr>',file=f)
+    for j in range(0,5):
+        k=k+1
+        if k<=n-1:
+            pic=imgl[k]
+            w1=200
+            h1=int(hh[k]/ww[k]*200)
+            print('<td align="center"><img src="'+pic+'" alt="pic" width="'+str(w1)+'", height="'+str(h1)+'"><br><a href="'+pic+'">I'+pic+'<a></td>',file=f)
+        else:
+            print('<td></td>',file=f)
+    print('</tr>',file=f)
+print('</table>',file=f)
+print('</body>',file=f)
+print('</html>',file=f)
+f.close()
 
 # ファイルを受け取る方法の指定
 @app.route('/', methods=['GET', 'POST'])
 def uploads_file():
     # リクエストがポストかどうかの判別
     if request.method == 'POST':
-        # ファイルがなかった場合の処理
-        if 'pic' not in request.files:
-            print('ファイルがありません')
-            return redirect(request.url)
         # データの取り出し
-        imgfile = request.files['pic']
-        print(imgfile)
-        # ファイル名がなかった時の処理
-        if imgfile.filename == '':
-            print('ファイルがありません')
-            return redirect(request.url)
-        # ファイルのチェック
-        if imgfile and allwed_file(imgfile.filename):
-            # 危険な文字を削除（サニタイズ処理）
-            filename = secure_filename(imgfile.filename)
-            # ファイルの保存
-            imgfile.save(os.path.join("..","uploads", filename))
-            # アップロード後のページに転送
-            return redirect(url_for('uploaded_file', filename=filename))
+        # imgfolder = request.files['pic']
+        for filename in os.listdir(path):
+            if os.path.isfile(os.path.join(path, filename)): #ファイルのみ取得
+                files.append(filename)
+        
     if request.method == 'GET':
         return render_template('index.html')
 
@@ -50,12 +88,6 @@ def uploads_file():
 # ファイルを表示する
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
-
-# Flaskとwtformsを使い、index.html側で表示させるフォームを構築する
-class Form(Form):
-
-    # html側で表示するsubmitボタンの表示
-    submit = SubmitField("判定")
 
 if __name__ == "__main__":
     app.run()
