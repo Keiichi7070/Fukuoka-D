23533a5dd86879e51d57484a5559026fa81d5207
keiichi7070 <keyiii7070@gmail.com>
2019-12-19 17:29:00 +0900

    pages5

--- pages/app/app.py
+++ pages/app/app.py
@@ -5,70 +5,17 @@ import os
 from werkzeug.utils import secure_filename
 from sklearn.externals import joblib
 import re
-from PIL import Image
+from create_img import analyze
+import glob
 
 # 画像のアップロード先のディレクトリ
-UPLOAD_FOLDER = '../uploads'
-# アップロードされる拡張子の制限
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'gif'])
+UPLOAD_FOLDER = './uploads'
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="uploads")
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-path = '../uploads'
-
-files = []
-
-for filename in os.listdir(path):
-         if os.path.isfile(os.path.join(path, filename)):  #ファイルのみ取得
-                     files.append(filename)
-
-def allwed_file(filename):
-    # .があるかどうかのチェックと、拡張子の確認
-    # OKなら１、だめなら0
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-
-filenames = os.listdir('./')
-imgl=[]
-ww=[]
-hh=[]
-for fname in sorted(filenames):
-    path, ext = os.path.splitext( os.path.basename(fname) )
-    if ext=='.JPG' and path[0:2]!='._':
-        pic=path+ext
-        im=Image.open(pic)
-        w=im.size[0]
-        h=im.size[1]
-        print(pic, w, h)
-        imgl=imgl+[pic]
-        ww=ww+[w]
-        hh=hh+[h]
-
-f=open('maggie.html','w')
-print('<html>',file=f)
-print('<body>',file=f)
-print('<table>',file=f)
-n=len(imgl)
-m=int(n/5)+1
-k=-1
-for i in range(0,m):
-    print('<tr>',file=f)
-    for j in range(0,5):
-        k=k+1
-        if k<=n-1:
-            pic=imgl[k]
-            w1=200
-            h1=int(hh[k]/ww[k]*200)
-            print('<td align="center"><img src="'+pic+'" alt="pic" width="'+str(w1)+'", height="'+str(h1)+'"><br><a href="'+pic+'">I'+pic+'<a></td>',file=f)
-        else:
-            print('<td></td>',file=f)
-    print('</tr>',file=f)
-print('</table>',file=f)
-print('</body>',file=f)
-print('</html>',file=f)
-f.close()
+path = './uploads'
 
 # ファイルを受け取る方法の指定
 @app.route('/', methods=['GET', 'POST'])
@@ -76,18 +23,22 @@ def uploads_file():
     # リクエストがポストかどうかの判別
     if request.method == 'POST':
         # データの取り出し
-        # imgfolder = request.files['pic']
-        for filename in os.listdir(path):
-            if os.path.isfile(os.path.join(path, filename)): #ファイルのみ取得
-                files.append(filename)
+        imgfolder = request.files.getlist('pic')
+        for img in imgfolder:
+            filename = str(os.path.split(img.filename)[1])
+            img.save(path + '/' + filename)
         
+        analyze()
+        analyzedFolder = []
+        analyzedImages = glob.glob('image/*.jpg')
+        for p in analyzedImages:
+            analyzedImagesName = p.filename
+            analyzedFolder.append(analyzedImagesName)
+
+        return render_template('result.html', images=analyzedFolder)
+
     if request.method == 'GET':
         return render_template('index.html')
-
-@app.route('/uploads/<filename>')
-# ファイルを表示する
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == "__main__":
     app.run()
---
+++ pages/app/create_img.py
@@ -0,0 +1,83 @@
+import os
+import keras
+import cv2
+import glob
+from PIL import Image
+from keras.utils import np_utils
+from shutil import copy
+from keras.layers.convolutional import Conv2D, MaxPooling2D
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.preprocessing.image import array_to_img, img_to_array, load_img
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import matplotlib.pyplot as plt
+import re
+
+def analyze():
+    def list_pictures(directory, ext='jpg'):
+        return [os.path.join(root, f)
+            for root, _, files in os.walk(directory) for f in files
+            if re.match(r'([\w]+\.(?:' + ext + '))', f.lower())]
+
+    # モデルの読込
+    # 保存したjsonファイルとhdf5ファイルを読み込む。モデルを学習に使うにはcompileが必要。
+    from keras.models import model_from_json
+
+    # JSON形式のデータを読み込んでモデルとして復元。学習で使うにはまたコンパイルが必要なので注意。
+    with open('select_img2.model', 'r') as f:
+        json_string = f.read()
+    model = model_from_json(json_string)
+
+    # モデルにパラメータを読み込む。前回の学習状態を引き継げる。
+    model.load_weights('param2.hdf5')
+    print('Loaded the model.')
+
+    # コンパイル
+    model.compile(loss='categorical_crossentropy',
+                optimizer='SGD',
+                metrics=['accuracy'])
+
+    Xj = []
+    Yj = []
+    num = [] #何番目の画像がぶれているかを保存
+    a = [1] #ぶれている画像との比較用
+    i = 0 #画像の番号
+
+    #画像フォルダ(img_file/test)の読み込み
+    for picture in list_pictures('./uploads'): 
+        img = img_to_array(load_img(picture))
+        re_img = cv2.resize(img, (64, 64))
+        Xj.append(re_img)
+        Yj.append(1)
+        
+    # arrayに変換
+    Xj = np.asarray(Xj)
+    Yj = np.asarray(Yj)
+        
+    # 画素値を0から1の範囲に変換
+    Xj = Xj.astype('float32')
+    Xj = Xj / 255.0
+
+    # クラスの形式を変換
+    Yj = np_utils.to_categorical(Yj, 2)
+
+    # テストデータに適用
+    predict_classes = model.predict_classes(Xj)
+    for predict_test in predict_classes:
+        if np.allclose(predict_test, a) == True:
+            num.append(i)
+        i += 1
+    # マージ。yのデータは元に戻す
+    mg_df = pd.DataFrame({'predict': predict_classes, 'class': np.argmax(Yj, axis=1)})
+    # confusion matrix
+    pd.crosstab(mg_df['class'], mg_df['predict'])   
+
+    p = 0 #画像の番号
+    os.makedirs('image', exist_ok=True) #imageというフォルダの作成
+    for path in glob.glob(os.path.join('uploads', '*.jpg')): #画像の読み込み
+        if p in num: 
+            copy(path,'{}/{}.jpg'.format('image',str(p).zfill(5))) #画像ファイルのコピー
+            basename = os.path.basename('uploads')
+        p += 1
